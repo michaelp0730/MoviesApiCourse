e61ab80a6538f2d77c6d8f60f657a82908302c96
Michael Pellegrini <michael@cappers.ai>
2024-11-21 14:26:55 -0800

    Adds RateMovieAsync method

---
+++ Movies.Application/Services/RatingService.cs
@@ -0,0 +1,32 @@
+using FluentValidation.Results;
+using Movies.Application.Repositories;
+
+namespace Movies.Application.Services;
+
+public class RatingService : IRatingService
+{
+    private readonly IMovieRepository _movieRepository;
+    private readonly IRatingRepository _ratingRepository;
+
+    public RatingService(IMovieRepository movieRepository, IRatingRepository ratingRepository)
+    {
+        _movieRepository = movieRepository;
+        _ratingRepository = ratingRepository;
+    }
+
+    public async Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default)
+    {
+        if (rating is <= 0 or > 5)
+        {
+            throw new FluentValidation.ValidationException(new List<ValidationFailure>
+            {
+                new ValidationFailure("Rating", "Rating must be between 1 and 5")
+            });
+        }
+
+        var movieExists = await _movieRepository.ExistsByIdAsync(movieId, token);
+        if (!movieExists) return false;
+
+        return await _ratingRepository.RateMovieAsync(movieId, rating, userId, token);
+    }
+}
